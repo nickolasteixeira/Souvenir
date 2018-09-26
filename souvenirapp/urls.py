42a0a9916b25ab462d6ca979e59d7bef4d6c1e4b
gjdame <gregoryjdame@gmail.com>
2018-09-26 20:53:45 +0000

    update login and add create and review templates

--- souvenirapp/urls.py
+++ souvenirapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/<int:user_id>', views.create, name="create_itinerary"),
+    path('review/<int:user_id>', views.review, name="write_review"),
 ]
--- souvenirapp/views.py
+++ souvenirapp/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+def create(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'souvenirapp/create.html', {'user': user})
+def review(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return HttpResponse("You are at the create review page %s" % user.username)
