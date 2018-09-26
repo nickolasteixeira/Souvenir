0014e3f425051fed75880706f51d27d38784b830
Dkazem91 <Dkazemian@gmail.com>
2018-09-26 16:36:25 -0700

    created new branch pushed search functionality rough draft

--- souvenirapp/urls.py
+++ souvenirapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/<int:user_id>', views.create, name="create_itinerary"),
     path('review/<int:user_id>', views.review, name="write_review"),
+    path('search/<str:user_id>', views.search, name="search"),
 ]
--- souvenirapp/views.py
+++ souvenirapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.template.loader import get_template
+from django.template import Context
+from .models import City, Place, Person, Review
 
 # Create your views here.
 from django.http import HttpResponse
@@ -13,3 +16,22 @@ def create(request, user_id):
 def review(request, user_id):
     user = get_object_or_404(User, id=user_id)
     return HttpResponse("You are at the create review page %s" % user.username)
+
+def search(request, user_id):
+    query = request.POST.get('usr_query', False)
+    print ("QUERY: ", query)
+    t = get_template('souvenirapp/search_results.html')
+    city = City.objects.get(name=query).id
+    print("CITY ID", city)
+    friendsList = Person.objects.get(user_id=user_id).get_friends()
+    print("FRIENDS LIST", friendsList)
+    reviews = []
+    for friend in friendsList:
+        rList = Review.objects.filter(user_id_id=friend)
+        user = User.objects.get(id=friend)
+        for rev in rList:
+            place = Place.objects.get(id=rev.place_id_id)
+            if place.city_id == city:
+                reviews.append((rev,place,user))
+    print("REVIEWS", reviews)
+    return HttpResponse(t.render({'query':reviews}))
