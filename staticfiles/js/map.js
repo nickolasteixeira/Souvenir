28fbfd10b6df6f058a1214c792732b314318a6f5
gjdame <gregoryjdame@gmail.com>
2018-10-02 00:48:35 +0000

    add map functionality for searching

---
+++ staticfiles/js/map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2pkYW1lIiwiYSI6ImNqbXFsNXhnYzBpd2gzbGxoY3lwamRuYWUifQ.OFmEoBXX0QrvFMqe8qcmEw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});
