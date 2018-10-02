52a158df6faf690c5c4eb84e2078042d79f7a473
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-10-02 19:59:35 +0000

    initial datadump

---
+++ datadump.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import requests
+import os
+import psycopg2
+
+def dump_data():
+    '''data dump from foursquare api'''
+    client_id = os.environ.get('CLIENT_ID')
+    client_secret = os.environ.get('CLIENT_SECRET')
+    url = 'https://api.foursquare.com/v2/venues/explore?near=San_Francisco&section=food&client_id=' + client_id + '&client_secret=' + client_secret + '&v=20180924'
+    r = requests.get(url)
+    if r.status_code == 200:
+        conn = psycopg2.connect('dbname=testpython user=vagrant123')
+        cur = conn.cursor()
+        data = r.json()
+        for item in data.get('response').get('groups')[0].get('items'):
+            category = 'Eat'
+            name = item.get('venue').get('name')
+            description = item.get('venue').get('categories')[0].get('name')
+            lat = item.get('venue').get('location').get('lat')
+            lng = item.get('venue').get('location').get('lng')
+            zipcode = item.get('venue').get('location').get('postalCode')
+            address = item.get('venue').get('location').get('address')
+            item_id = item.get('venue').get('id')
+            phone = 
+            photoUrl = ''
+            #request 2 to get photoURL
+            url2 = 'https://api.foursquare.com/v2/venues/' + item_id + '/photos?client_id=' + client_id + '&client_secret=' + client_secret + '&v=20180924'
+            r2 = requests.get(url2)
+            if r2.status_code == 200:
+                data2 = r2.json()
+                for photo in data2.get('response').get('photos').get('items'):
+                    photoUrl += photo.get('prefix') + '400x300' + photo.get('suffix')
+            else:
+                print('Unable to retrieve photo url => Statu_code {}'.format(r2.status_code))            
+            
+            '''Category, Name, Description, Lat, Long, Zip, Address, Phone, Photourl'''
+            cur.execute(
+            'INSERT INTO souvenirapp_place VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+            (category, name, description, lat, lng, zipcode, address, phone, photoUrl) 
+            )
+            #printing
+            '''
+            print('Category: {}'.format(category))
+            print('Name: {}'.format(name))
+            print('Description: {}'.format(description))
+            print('Lat: {}'.format(lat))
+            print('Long: {}'.format(lng))
+            print('Zipcode: {}'.format(zipcode))
+            print('Address: {}'.format(address))
+            print('Phone: {}'.format('empty'))
+            print('PhotoURL: {}'.format(photoUrl))
+            print('-----------------')
+            print()
+            '''
+    else:
+        print("Unable to retrieve data -> Status Code: {}".format(r.status_code))
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    dump_data()
