8289d30f06ba00ef16ab341eaf337cb774b9f19f
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-10-03 20:28:46 +0000

    all friends api

--- souvenirapp/urls.py
+++ souvenirapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api/city_list/', views.city_list, name='city_list'),
     path('api/<int:city_id>/places', views.places_list, name='places_list'),
     path('api/<str:city_name>/<int:user_id>/places', views.search, name='search'),
-    path('api/friends/<int:user_id>/<str:search>', views.search_friends, name='get_friends')
+    path('api/friends/<int:user_id>/<str:search>', views.search_friends, name='search_friends'),
+    path('api/allfriends/<int:user_id>', views.get_friends, name='get_friends')
 #    path('search/<str:user_id>', views.search, name="search"),
 ]
