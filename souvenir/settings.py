e8dc9fdc9cec0d83dfa5d2c3314859c56049f625
Nickolas Teixeira <NickolasTeixeira01@gmail.com>
2018-10-08 20:42:43 +0000

    upstart file for gunicorn instance

--- souvenir/settings.py
+++ souvenir/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'testpython',
-        'USER': 'vagrant',
+        'USER': 'ubuntu',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
